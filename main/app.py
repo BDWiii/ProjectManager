1b3012a6a774d4bbc9587e31ce2ef73baa4467dc
BDWI <badawiii.676@gmail.com>
2025-09-17 19:19:24 +0300

    Fix: server initialization

--- main/app.py
+++ main/app.py
@@ -109,4 +109,4 @@ if __name__ == "__main__":
     import uvicorn
 
     # Run the server
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, log_level="info")
+    uvicorn.run("main.app:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
